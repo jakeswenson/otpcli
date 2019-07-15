e6cf24d7daf4336d45dcb0742155d13d7200bab1
Jake Swenson <jake@jakeswenson.com>
2019-07-14 17:10:35 -0700

    build clean up

--- .ci/setup_deploy.sh
+++ .ci/setup_deploy.sh
@@ -4,4 +4,7 @@ mkdir deploy
 set -x
 
 cp target/release/otpcli "deploy/otpcli-$DEPLOY_NAME"
-tar -cJf "deploy/otpcli.$DEPLOY_NAME.tar.xz" "deploy/otpcli-$DEPLOY_NAME"
+tar -cJf "deploy/otpcli.$DEPLOY_NAME.tar.xz" -C target/release otpcli
+
+ls -lah target/release
+ls -lah deploy
