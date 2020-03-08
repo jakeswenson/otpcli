186d92d596c7c30a269aa45e91091fb1ac79d4f6
Jake Swenson <jake@jakeswenson.com>
2020-03-08 11:54:00 -0700

    update deps

--- src/cli.rs
+++ src/cli.rs
@@ -5,7 +5,7 @@ use otp::{TotpError, TotpResult};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
-#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+#[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
 pub struct Options {
     #[structopt(subcommand)]
     pub cmd: Option<Command>,
@@ -31,11 +31,11 @@ pub struct Options {
 pub enum Command {
     /// Add/Update a new TOTP secret
     #[structopt(name = "add")]
-    #[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+    #[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
     AddSecret { name: String, secret: String },
     /// Import an RSAToken into otpcli
     #[structopt(name = "import")]
-    #[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+    #[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
     ImportStoken {
         name: String,
         #[structopt(parse(from_os_str))]
@@ -44,20 +44,20 @@ pub enum Command {
     },
     /// Add/Update a new TOTP secret
     #[structopt(name = "list")]
-    #[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+    #[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
     ListSecrets { prefix: Option<String> },
     /// Add/Update a new TOTP secret
     #[structopt(name = "delete")]
-    #[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+    #[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
     DeleteSecret { name: String },
     /// Migrate secrets stored in the config to be stored in the keychain
     #[cfg(feature = "keychain")]
     #[structopt(name = "migrate-to-keychain")]
-    #[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+    #[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
     UseKeychain,
     /// Generate a token
     #[structopt(name = "generate")]
-    #[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+    #[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
     GenerateToken { name: String },
 }
 
